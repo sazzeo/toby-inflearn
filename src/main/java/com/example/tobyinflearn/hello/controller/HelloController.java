10eff3ab44da6069332418709a53d5a3ed43d23a
jiyeong-dt <imjee02@gmail.com>
2023-07-18 00:18:17 +0900

    refactor: 코드 정리

--- src/main/java/com/example/tobyinflearn/hello/controller/HelloController.java
+++ src/main/java/com/example/tobyinflearn/hello/controller/HelloController.java
@@ -2,17 +2,15 @@ package com.example.tobyinflearn.hello.controller;
 
 import com.example.tobyinflearn.hello.service.HelloService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Objects;
 
 @Component
 @RequestMapping("/")
 public class HelloController {
     private final HelloService helloService;
+
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
--- src/main/java/com/example/tobyinflearn/hello/service/SimpleHelloService.java
+++ src/main/java/com/example/tobyinflearn/hello/service/SimpleHelloService.java
@@ -1,7 +1,8 @@
 package com.example.tobyinflearn.hello.service;
 
-import org.springframework.stereotype.Component;
-@Component
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     public String sayHello(String name) {
         return "hello " + name;
